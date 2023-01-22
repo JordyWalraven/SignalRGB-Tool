49f0a4731f4210716443a1834a8e59f7ea73328a
Jordy Walraven <jordywalraven1@gmail.com>
2023-01-22 17:03:18 +0100

    Adding fixes

--- core/Core/classes/ProcessManager.cs
+++ core/Core/classes/ProcessManager.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Management;
 
 namespace Core;
 
@@ -37,7 +38,23 @@ class ProcessManager
       {
         return "error";
       }
-      Process.Start(signalpath).Start();
+
+      using (var managementClass = new ManagementClass("Win32_Process"))
+      {
+        var processInfo = new ManagementClass("Win32_ProcessStartup");
+        processInfo.Properties["CreateFlags"].Value = 0x00000008;
+
+        var inParameters = managementClass.GetMethodParameters("Create");
+        inParameters["CommandLine"] = signalpath;
+        inParameters["ProcessStartupInformation"] = processInfo;
+
+        var result = managementClass.InvokeMethod("Create", inParameters, null);
+        if ((result != null) && ((uint)result.Properties["ReturnValue"].Value != 0))
+        {
+          Console.WriteLine("Process ID: {0}", result.Properties["ProcessId"].Value);
+        }
+      }
+
       return "success";
     }
     else
